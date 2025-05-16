311f4768afa8d4df6bda9d622b2990246dcbec71
cedmoh <shockylemi@gmail.com>
2025-05-16 16:40:06 +0200

    fix: add egui

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use bevy_inspector_egui::bevy_egui::EguiPlugin;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use strategy::arrow::ArrowPlugin;
 use strategy::soldier::SoldierPlugin;
 
@@ -6,8 +8,12 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugins(SoldierPlugin)
         .add_plugins(ArrowPlugin)
+        .add_plugins(SoldierPlugin)
+        .add_plugins(EguiPlugin {
+            enable_multipass_for_primary_context: true,
+        })
+        .add_plugins(WorldInspectorPlugin::new())
         .add_systems(Startup, add_ui)
         .run();
 }
