8eaef98fd978527278251d782e7ff9e9d84912ed
Bradford Hovinen <bradford@hovinen.tech>
2025-05-16 11:23:51 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Update, hello_world)
+        .run();
+}
+
+fn hello_world(mut commands: Commands) {
+    commands.spawn(());
+    println!("Hello, world!");
+}
